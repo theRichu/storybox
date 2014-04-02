ecf540ae41d44e8c80669314adba9c87ff74975b
root <root@ubuntu.(none)>
2014-04-02 14:29:42 +0900

    room

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../YiiRoot/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
+$yii = dirname(__FILE__) . '/../YiiRoot/framework/yii.php';
+$config = dirname(__FILE__) . '/protected/config/main.php';
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_DEBUG') or define('YII_DEBUG', true);
 // specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 
-require_once($yii);
+require_once ($yii);
 
 // Yii::createWebApplication($config)->run();
 
 $app = Yii::createWebApplication($config);
-if (YII_DEBUG){
-	Yii::import("application.components.FirePHPCore.fb", true);
+if (YII_DEBUG) {
+    Yii::import("application.components.FirePHPCore.fb", true);
 }
 $app->run();
--- protected/migrations/m140401_025559_create_user_table.php
+++ protected/migrations/m140401_025559_create_user_table.php
@@ -2,34 +2,28 @@
 
 class m140401_025559_create_user_table extends CDbMigration
 {
-	public function up()
-	{
-		$this->createTable('tbl_user', array(
-				'id' => 'pk',
-				'username' => 'string NOT NULL',
-				'email' => 'string NOT NULL',
-				'password' => 'string NOT NULL',
-				'create_time' => 'datetime DEFAULT NULL',
-				'last_login_time' => 'datetime DEFAULT NULL',
-				'update_time' => 'datetime DEFAULT NULL',
-				'mobile' => 'string DEFAULT NULL',
-				'phone' => 'string DEFAULT NULL',
-		), 'ENGINE=InnoDB');		
-	}
 
-	public function down()
-	{
-		$this->dropTable('tbl_user');
-	}
+    public function up()
+    {
+        $this->createTable('tbl_user', array(
+            'id' => 'pk',
+            'username' => 'string NOT NULL',
+            'email' => 'string NOT NULL',
+            'password' => 'string NOT NULL',
+            'create_time' => 'datetime DEFAULT NULL',
+            'last_login_time' => 'datetime DEFAULT NULL',
+            'update_time' => 'datetime DEFAULT NULL',
+            'mobile' => 'string DEFAULT NULL',
+            'phone' => 'string DEFAULT NULL'
+        ), 'ENGINE=InnoDB');
+    }
 
-	/*
-	// Use safeUp/safeDown to do migration with transaction
-	public function safeUp()
-	{
-	}
-
-	public function safeDown()
-	{
-	}
-	*/
+    public function down()
+    {
+        $this->dropTable('tbl_user');
+    }
+    
+    /*
+     * // Use safeUp/safeDown to do migration with transaction public function safeUp() { } public function safeDown() { }
+     */
 }
